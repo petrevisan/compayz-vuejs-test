e84ee27bba36f2de849e3123990dce86c8e140d1
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2024-04-15 10:25:51 -0400

    remove: cors + middleware

--- middleware/cors.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Sua lógica da função continua aqui...
-};
