1bd48d53a77dd9d4f962e6300701ef70cfc82b44
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2024-04-15 10:21:22 -0400

    att: cors.js

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,7 @@
-export default function ({ app, req, res }) {
-    // Adicione os cabeçalhos CORS necessários aqui
+module.exports = (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-}
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Sua lógica da função continua aqui...
+};
