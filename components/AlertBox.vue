ad30ef1f7f0b01a6180748dcda805a248a57ff28
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2024-04-15 16:17:39 -0400

    add: alertbox

---
+++ components/AlertBox.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div id="alert">
+            <span>Plano indisponível no momento.</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AlertBox'
+}
+</script>
