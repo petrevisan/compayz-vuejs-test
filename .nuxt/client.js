b367affc3a5f44eb6476b6a4c63f50de7fec45ae
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2024-04-15 10:25:54 -0400

    remove: cors + middleware

--- .nuxt/client.js
+++ .nuxt/client.js
@@ -230,7 +230,7 @@ function resolveComponents (route) {
 }
 
 function callMiddleware (Components, context, layout, renderState) {
-  let midd = ["cors"]
+  let midd = []
   let unknownMiddleware = false
 
   // If layout is undefined, only call global middleware
--- .nuxt/middleware.js
+++ .nuxt/middleware.js
@@ -1,6 +1,3 @@
 const middleware = {}
 
-middleware['cors'] = require('../middleware/cors.js')
-middleware['cors'] = middleware['cors'].default || middleware['cors']
-
 export default middleware
