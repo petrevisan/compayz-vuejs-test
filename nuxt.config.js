9192a851224c4a47f477c8188a790b40d61e9f89
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2024-04-19 17:57:13 -0400

    fix: change baseUrl

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,6 +54,11 @@ export default {
     build: {
     },
 
+    axios: {
+        // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+        baseURL: '/'
+    },
+
     security: {
         corsHandler: {
             origin: '*', // Permitir todas as origens
